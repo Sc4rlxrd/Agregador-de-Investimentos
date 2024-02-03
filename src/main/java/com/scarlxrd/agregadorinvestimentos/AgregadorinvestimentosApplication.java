3ddbdf9849ba85444defad1330982c5ce4e104c8
Guilherme Santos <guilhermesantos2004gss@gmail.com>
2024-02-03 14:17:54 -0300

    feat: confi project

---
+++ src/main/java/com/scarlxrd/agregadorinvestimentos/AgregadorinvestimentosApplication.java
@@ -0,0 +1,13 @@
+package com.scarlxrd.agregadorinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorinvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorinvestimentosApplication.class, args);
+	}
+
+}
